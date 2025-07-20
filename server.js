55d850601c75da579b330bc3655794cc75635be8
S M Masrafi <smmasrafi000@gmail.com>
2025-07-21 00:23:39 +0600

    Updated

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import fastify from "fastify";
 import dotenv from "dotenv";
+import fastify from "fastify";
 import sendMailRoute from "./src/routes/sendMail.js";
 
 dotenv.config();
@@ -9,7 +9,7 @@ const app = fastify({
   });
 
   app.register(sendMailRoute);
-  
+  // Health check
   app.get("/", async (request, reply) => {
     return { hello: "world" };
   });
