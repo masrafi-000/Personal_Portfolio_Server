6b3482389e4d282a5fbbb39eceb4f82c6d437927
S M Masrafi <smmasrafi000@gmail.com>
2025-07-21 01:08:17 +0600

    Updated

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ import sendMailRoute from "./src/routes/sendMail.js";
 
 dotenv.config();
 
-const fastify = fastify({
-    logger: true,
-  });
+const app = fastify({
+  logger: true,
+});
 
-  fastify.register(sendMailRoute);
-  // Health check
-  fastify.get("/", async (request, reply) => {
-    return { hello: "world" };
-  });
-  
-  const start = async () => {
-    const PORT = process.env.PORT || 5000;
-    try {
-      await fastify.listen({ port: PORT });
-    } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  };
-  
-  start();
-  
+app.register(sendMailRoute);
+
+// Health check
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  const PORT = process.env.PORT || 5000;
+  try {
+    await app.listen({ port: PORT, host: "0.0.0.0" }); 
+  } catch (err) {
+    app.log.error(err)
+    process.exit(1);
+  }
+};
+
+start();
